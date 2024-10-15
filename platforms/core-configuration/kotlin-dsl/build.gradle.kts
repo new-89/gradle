89f87e331e1947e62f1336bcc68238cb12ca3733
József Bartók <jbartok@gradle.com>
2024-10-15 10:53:06 +0300

    Fix issues caused by rebasing onto `master`

--- platforms/core-configuration/kotlin-dsl/build.gradle.kts
+++ platforms/core-configuration/kotlin-dsl/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     api(projects.hashing)
     api(projects.kotlinDslToolingModels)
     api(projects.loggingApi)
+    api(projects.persistentCache)
     api(projects.stdlibJavaExtensions)
     api(projects.toolingApi)
 
@@ -43,8 +44,8 @@ dependencies {
     implementation(projects.messaging)
     implementation(projects.modelCore)
     implementation(projects.normalizationJava)
-    implementation(projects.persistentCache)
     implementation(projects.resources)
+    implementation(projects.serialization)
     implementation(projects.serviceLookup)
     implementation(projects.serviceProvider)
     implementation(projects.snapshots)
@@ -56,7 +57,7 @@ dependencies {
     implementation(libs.groovyJson)
     implementation(libs.kotlinReflect)
 
-    runtimeOnly(libs.futureKotlin("build-tools-impl")) {
+    runtimeOnly(libs.kotlinBuildToolsImpl) {
         isTransitive = false
     }
 
